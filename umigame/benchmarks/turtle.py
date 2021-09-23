18e3d978153ed15ef84a2581c0529b36099b53bb
penguinwang96825 <penguinwang@smail.nchu.edu.tw>
2021-09-23 15:04:31 +0100

    update SL constraints

--- umigame/benchmarks/turtle.py
+++ umigame/benchmarks/turtle.py
@@ -33,9 +33,19 @@ class TurtleStrategy(BaseStrategy):
             timeperiod=window_atr
         )
         
-    def run(self, capital=10000, fee=0.001, position=0, verbose=False):
+    def run(self, capital=10000, fee=0.001, stop_loss=0.1, take_profit=0.5, position=0, verbose=False):
         entry, exit = self.generate_signals(self.universe)
-        self.trades = self.trade(self.universe, entry, exit, capital, fee, position, verbose)
+        self.statements = self.trade(
+            self.universe, 
+            entry, 
+            exit, 
+            capital, 
+            fee, 
+            stop_loss, 
+            take_profit, 
+            position, 
+            verbose
+        )
 
     def generate_signals(self, universe):
         # Compute signal based on Donchian Channel
@@ -45,12 +55,50 @@ class TurtleStrategy(BaseStrategy):
         entry, exit = entry[~np.isnan(entry)], exit[~np.isnan(exit)]
         return entry, exit
 
-    def trade(self, universe, entry, exit, capital=10000, fee=0.001, position=0, verbose=True):
+    def trade(
+            self, 
+            universe, 
+            entry, exit, 
+            capital=10000, 
+            fee=0.001, 
+            stop_loss=0.1, 
+            take_profit=0.5, 
+            position=0, 
+            verbose=True
+        ):
         universe.loc[(entry==1).index, 'signal'] = 1
         universe.loc[(exit==1).index, 'signal'] = 0
         universe["value"] = capital
         balance = capital
+        buy_price, sell_price = None, None
         for idx, row in universe.iterrows():
+            # Stop loss
+            if (buy_price is not None) and (stop_loss is not None) and (row["close"] < buy_price*(1.0-stop_loss)):
+                stop_loss_price = row["close"]
+                stop_loss_date = pd.to_datetime(idx).date()
+                if row["close"] < buy_price-2*row["atr"]:
+                    balance += stop_loss_price * position * (1-fee)
+                    position -= position
+                else:
+                    balance += stop_loss_price * math.floor(position * 0.5) * (1-fee)
+                    position -= math.floor(position * 0.5)
+                if verbose:
+                    print(f"Stop loss at price {stop_loss_price:.4f} on {stop_loss_date}")
+                    print(f"Last buy at {buy_price:.4f}")
+                    print(f"Balance: {balance:.4f} Position: {position:.4f}")
+                    print("-"*50)
+            # Take profit
+            elif (buy_price is not None) and (take_profit is not None) and (row["close"] >= buy_price*(1.0+take_profit)):
+                take_profit_price = row["close"]
+                take_profit_date = pd.to_datetime(idx).date()
+                balance += take_profit_price * math.floor(position * 0.5) * (1-fee)
+                position -= math.floor(position * 0.5)
+                if verbose:
+                    print(f"Take profit at price {take_profit_price:.4f} on {take_profit_date}")
+                    print(f"Last buy at {take_profit_date:.4f}")
+                    print(f"Balance: {balance:.4f} Position: {position:.4f}")
+                    print("-"*50)
+
             if row["signal"] == 1:
                 if balance > 0:
                     buy_price = row["close"]
@@ -78,6 +126,44 @@ class TurtleStrategy(BaseStrategy):
             universe.loc[idx, "position"] = position
         return universe
 
+    def plot(self):
+
+        def align_xticks_for_df(df, baseline):
+            return df.reindex(baseline.index).bfill()
+
+        entry_date = self.universe[self.universe['signal']==1].index
+        exit_date = self.universe[self.universe['signal']==0].index
+
+        fig = plt.figure(figsize=(15, 8), constrained_layout=True)
+        gs = gridspec.GridSpec(6, 3, figure=fig)
+        ax1 = plt.subplot(gs[0:2, :])
+        ax2 = plt.subplot(gs[2:4, :])
+        ax3 = plt.subplot(gs[4, :])
+        ax4 = plt.subplot(gs[5, :])
+
+        ax1.plot(self.universe.close, label="Close Price")
+        ax1.plot(self.universe['dc_high'], '#B97A95', label="DC-HIGH", alpha=1, linestyle="--")
+        ax1.plot(self.universe['dc_low'], '#F6AE99', label="DC-LOW", alpha=1, linestyle="--")
+        ax1.scatter(entry_date, self.universe.close[entry_date], label="buy", marker="^", alpha=.7, color="tab:green")
+        ax1.scatter(exit_date, self.universe.close[exit_date], label="sell", marker="v", alpha=.7, color="tab:red")
+        ax1.set_title("Entry and exit signals")
+        ax1.legend(loc="upper left")
+        ax1.grid()
+
+        ax2.plot(align_xticks_for_df(self.universe['value'], self.universe))
+        ax2.set_title("Portfolio value")
+        ax2.grid()
+
+        ax3.plot(align_xticks_for_df(self.universe['balance'], self.universe))
+        ax3.set_title("Balance")
+        ax3.grid()
+
+        ax4.plot(align_xticks_for_df(self.universe['position'], self.universe))
+        ax4.set_title("Position")
+        ax4.grid()
+
+        plt.show()
+
 
 class DonchianChannelStrategy(BaseStrategy):
 
@@ -98,7 +184,7 @@ class DonchianChannelStrategy(BaseStrategy):
         
     def run(self, capital=10000, fee=0.001, position=0, quota=0.5, stop_loss=0.1, take_profit=0.5, verbose=False):
         entry, exit = self.generate_signals(self.universe)
-        self.trades = self.trade(
+        self.statements = self.trade(
             self.universe, 
             entry, exit, 
             capital, 
@@ -142,20 +228,22 @@ class DonchianChannelStrategy(BaseStrategy):
                 stop_loss_date = pd.to_datetime(idx).date()
                 balance += stop_loss_price * math.floor(position * 0.5) * (1-fee)
                 position -= math.floor(position * 0.5)
-                print(f"Stop loss at price {stop_loss_price:.4f} on {stop_loss_date}")
-                print(f"Last buy at {buy_price:.4f}")
-                print(f"Balance: {balance:.4f} Position: {position:.4f}")
-                print("-"*50)
+                if verbose:
+                    print(f"Stop loss at price {stop_loss_price:.4f} on {stop_loss_date}")
+                    print(f"Last buy at {buy_price:.4f}")
+                    print(f"Balance: {balance:.4f} Position: {position:.4f}")
+                    print("-"*50)
             # Take profit
             elif (buy_price is not None) and (take_profit is not None) and (row["close"] >= buy_price*(1.0+take_profit)):
                 take_profit_price = row["close"]
                 take_profit_date = pd.to_datetime(idx).date()
                 balance += take_profit_price * math.floor(position * 0.5) * (1-fee)
                 position -= math.floor(position * 0.5)
-                print(f"Take profit at price {take_profit_price:.4f} on {take_profit_date}")
-                print(f"Last buy at {take_profit_date:.4f}")
-                print(f"Balance: {balance:.4f} Position: {position:.4f}")
-                print("-"*50)
+                if verbose:
+                    print(f"Take profit at price {take_profit_price:.4f} on {take_profit_date}")
+                    print(f"Last buy at {take_profit_date:.4f}")
+                    print(f"Balance: {balance:.4f} Position: {position:.4f}")
+                    print("-"*50)
 
             if row["signal"] == 1:
                 if balance > 0:
