23e269533ebdba3c7acefc912c65fa1dd633f594
penguinwang96825 <penguinwang@smail.nchu.edu.tw>
2021-09-24 22:22:39 +0100

    add examples

--- umigame/nlp/attention.py
+++ umigame/nlp/attention.py
@@ -1,3 +1,4 @@
+import math
 import torch
 import torch.nn as nn
 
@@ -16,7 +17,7 @@ class NonparametricSelfAttention(nn.Module):
             ]
         ])
     >>> context_, attention_weights = NonparametricSelfAttention(3)(context)
-    >>> print("Input: ", sentences.shape)
+    >>> print("Input: ", context_.shape)
     Input:  torch.Size([1, 5, 3])
     >>> print("Output: ", context_.shape)
     Output:  torch.Size([1, 5, 3])
@@ -55,7 +56,7 @@ class ParametricSelfAttention(nn.Module):
             ]
         ])
     >>> context_, attention_weights = ParametricSelfAttention(3)(context)
-    >>> print("Input: ", sentences.shape)
+    >>> print("Input: ", context_.shape)
     Input:  torch.Size([1, 5, 3])
     >>> print("Output: ", context_.shape)
     Output:  torch.Size([1, 5, 3])
@@ -65,17 +66,22 @@ class ParametricSelfAttention(nn.Module):
         self.dimensions = dimensions
         self.softmax = nn.Softmax(dim=-1)
         self.tanh = nn.Tanh()
-        self.linear_in = nn.Linear(dimensions, dimensions, bias=False)
+        self.linear_q_in = nn.Linear(dimensions, dimensions, bias=False)
+        self.linear_k_in = nn.Linear(dimensions, dimensions, bias=False)
+        self.linear_v_in = nn.Linear(dimensions, dimensions, bias=False)
         self.linear_out = nn.Linear(dimensions, dimensions, bias=False)
 
     def forward(self, context, return_weights=True):
         """
         context: [sequence_length, embedding_dimension]
         """
-        context_ = self.linear_in(context)
-        attention_scores  = torch.bmm(context_, context_.transpose(1, 2))
-        attention_weights = self.softmax(attention_scores )
-        context_ = torch.bmm(attention_weights, context_)
+        context_q = self.linear_q_in(context)
+        context_k = self.linear_k_in(context)
+        context_v = self.linear_v_in(context)
+
+        attention_scores  = torch.bmm(context_q, context_k.transpose(1, 2))
+        attention_weights = self.softmax(attention_scores / math.sqrt(self.dimensions))
+        context_ = torch.bmm(attention_weights, context_v)
         context_ = self.tanh(context_)
         context_ = self.linear_out(context_)
         if return_weights:
@@ -84,3 +90,18 @@ class ParametricSelfAttention(nn.Module):
 
     def count_parameters(self):
         return sum(p.numel() for p in self.parameters() if p.requires_grad)
+
+
+if __name__ == "__main__":
+    context = torch.Tensor([
+            [
+                [0.6, 0.2, 0.8], 
+                [0.2, 0.3, 0.1], 
+                [0.9, 0.1, 0.8], 
+                [0.4, 0.1, 0.4], 
+                [0.4, 0.1, 0.6]
+            ]
+        ])
+    context_, attention_weights = ParametricSelfAttention(3)(context)
+    print("Input: ", context_.shape)
+    print("Output: ", context_.shape)
