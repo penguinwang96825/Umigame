363d6b449d27076cc561110407a319cf5cb32918
penguinwang96825 <penguinwang@smail.nchu.edu.tw>
2021-09-25 01:44:43 +0100

    add textcnn textlstm textbert

--- umigame/nlp/nnets.py
+++ umigame/nlp/nnets.py
@@ -1,8 +1,99 @@
 import math
 import torch
+import transformers
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
+
+
+class TextBERT(nn.Module):
+
+    def __init__(self, num_classes=2, dropout=0.3, freeze=True):
+        super().__init__()
+        self.bert = transformers.BertModel.from_pretrained(
+            "bert-base-uncased", return_dict=False
+        )
+        self.bert_drop = nn.Dropout(dropout)
+        self.out = nn.Linear(768, num_classes)
+        if freeze:
+            self.freeze_bert()
+
+    def forward(self, ids):
+        _, o_2 = self.bert(ids)
+        b_o = self.bert_drop(o_2)
+        output = self.out(b_o)
+        return output
+
+    def freeze_bert(self):
+        for param in self.bert.parameters():
+            param.requires_grad = False
+
+
+class TextCNN(nn.Module):
+    
+    def __init__(
+            self, 
+            vocab_size, 
+            embedding_dim, 
+            channel_dim, 
+            kernel_list, 
+            dropout, 
+            num_classes=2
+        ):
+        super(TextCNN, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embedding_dim)
+        # self.embed.weight.data.copy_(vocab_built.vectors)
+        self.convs = nn.ModuleList([nn.Conv2d(1, channel_dim, (w, embedding_dim)) for w in kernel_list])
+        self.dropout = nn.Dropout(dropout)
+        self.fc = nn.Linear(len(kernel_list)*channel_dim, num_classes)
+        
+    def forward(self, x):
+        emb_x = self.embed(x)
+        emb_x = emb_x.unsqueeze(1)
+        con_x = [conv(emb_x) for conv in self.convs]
+        pool_x = [F.max_pool1d(x.squeeze(-1), x.size()[2]) for x in con_x]
+        fc_x = torch.cat(pool_x, dim=1)
+        fc_x = fc_x.squeeze(-1)
+        fc_x = self.dropout(fc_x)
+        logit = self.fc(fc_x)
+        return logit
+
+
+class TextLSTM(nn.Module):
+
+    def __init__(
+            self, 
+            vocab_size, 
+            embedding_dim, 
+            hidden_dim, 
+            num_layers, 
+            dropout=0.1, 
+            bidirectional=True
+        ):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.lstm = nn.LSTM(
+            embedding_dim, 
+            hidden_dim, 
+            num_layers=num_layers, 
+            bidirectional=bidirectional, 
+            dropout=dropout,
+            batch_first=True
+        )
+        self.fc = nn.Linear(hidden_dim*2, 2)
+        self.act = nn.Sigmoid()
+        
+    def forward(self, text):
+        # embedded: [batch, seq_len, emb_dim]
+        embedded = self.embedding(text)
+        # hidden: [batch, num_layers * num_directions, hidden_dim]
+        # cell: [batch, num_layers * num_directions, hidden_dim]
+        packed_output, (hidden, cell) = self.lstm(embedded)
+        # hidden: [batch, hidden_dim * num_directions]
+        hidden = torch.cat((hidden[-2, :, :], hidden[-1, :, :]), dim=1)
+        # outputs: [batch, num_classes]
+        outputs = self.fc(hidden)
+        return outputs
 
 
 class Flatten(nn.Module):
