fcd442e8f199dc26f4dfa7807583c9d304b1e433
penguinwang96825 <penguinwang@smail.nchu.edu.tw>
2021-11-01 19:31:21 +0000

    update run script

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 rm -r -f lightning_logs
-CUDA_LAUNCH_BLOCKING=1 python umigame/experiments/run_text_classification.py
+# CUDA_LAUNCH_BLOCKING=1 python umigame/experiments/run_text_classification.py
+# CUDA_LAUNCH_BLOCKING=1 python umigame/experiments/run_textlstm.py
+CUDA_LAUNCH_BLOCKING=1 python umigame/experiments/run_fasttext.py
