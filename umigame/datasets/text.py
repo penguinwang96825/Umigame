ed963caee3c6fee6dfcdb077a5b06f6d99d9e579
penguinwang96825 <penguinwang@smail.nchu.edu.tw>
2021-11-07 21:47:14 +0000

    update load_dataframe

--- umigame/datasets/text.py
+++ umigame/datasets/text.py
@@ -1,31 +1,35 @@
 import os
 import pandas as pd
-import yfinance as yf
 from os.path import dirname
 from pathlib import Path
 from ..utils import column_name_lower
+from torch.utils.data import Dataset
 
 
 MODULE_PATH = Path(dirname(__file__))
 
 
-def load_dataset(name=None, split=None, split_time='2018'):
+def load_dataframe(source=None):
+    """
+    Parameters
+    ----------
+    source: str
+        Source of text dataframe. {'news', 'twitter', 'all'}
+    """
     file_path = os.path.join(MODULE_PATH, "text", "acl2021_news_and_twitter_datasets.csv")
     text_df = pd.read_csv(file_path)
     text_df = column_name_lower(text_df)
-    if name == 'news':
-        text_df = text_df.query("source=='news'").reset_index(drop=True)
-    elif name == 'twitter':
-        text_df = text_df.query("source=='twitter'").reset_index(drop=True)
 
-    if split == 'train':
-        train_text_df = text_df.loc[:split_time]
-        return train_text_df
-    elif split == 'test':
-        test_text_df = text_df.loc[split_time:]
-        return test_text_df
+    if source == 'news':
+        text_df = text_df.query("source=='news'").reset_index(drop=True)
+    elif source == 'twitter':
+        text_df = text_df.query("source=='twitter'").reset_index(drop=True)
+    elif source == 'all':
+        text_df = text_df.reset_index(drop=True)
     else:
-        return text_df
+        print('Please choose between "news", "twitter", and "all".')
+
+    return text_df
 
 
 def fetch_news():
